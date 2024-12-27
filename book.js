08b9a95f5d598fd0cb396edaee66ae94c0f393a9
Vrushabh Bhabire <80674108+Vrushabh-vb@users.noreply.github.com>
2024-12-27 18:46:46 +0530

    Update book.js

--- book.js
+++ book.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const pool = require("../db");
-const utils = require("../utils");
-const { BOOK_TABLE } = require("../config");
+const pool = require("./db");
+const utils = require("./utils");
+const { BOOK_TABLE } = require("./config");
 
 // Search book by title
 router.get("/title", (request, response) => {
